ed259321c339d5d52b8aa1dd8442ceeb1be59684
MCode <hashamdar.com@gmail.com>
2026-06-19 08:18:54 +0330

    Add files via upload

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -3,7 +3,7 @@ window.addEventListener('load', ()=>{
         const loading = document.querySelector(".loader-wrapper");
         loading.classList.add("dis-none")
         document.body.classList.remove("loading");
-    }, 1000);
+    }, 2000);
 });
 
 const card = document.getElementById('card');
